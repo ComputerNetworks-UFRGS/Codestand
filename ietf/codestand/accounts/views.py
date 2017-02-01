4e712d8ff56b8be54937aed54351bf25a64fbdbe
MatheusProla <matheusprola@hotmail.com>
2017-02-01 11:18:35 -0200

    Master with version 6.40.4 of Datatracker.

--- ietf/codestand/accounts/views.py
+++ ietf/codestand/accounts/views.py
@@ -15,30 +15,30 @@ def register(request):
     return HttpResponseRedirect(settings.CODESTAND_PREFIX + '/accounts/create/')
 
 
-def profile(request, user=None):
-    if user is None:
-        current_user = get_user(request)
-        if current_user is None:
-            raise Http404
-        else:
-            user = current_user.id
-    coder = Person.objects.using('datatracker').get(id=user)
-    email = Email.objects.using('datatracker').filter(person_id=user)
-    projects = ProjectContainer.objects.exclude(code_request__isnull=True).exclude(is_deleted=True).filter(owner=user)
-    codings = CodingProject.objects.filter(coder=user)
-    all_projects = ProjectContainer.objects.all()
-    selected_codings = []
-    for proj in all_projects:
-        all_codings = proj.codings.all()
-        for code in codings:
-            if code in all_codings:
-                selected_codings.append((proj, code))
-    return render_page(request, constants.TEMPLATE_PROFILE, {
-        'coder': coder,
-        'email': email,
-        'projects': projects,
-        'codings': selected_codings,
-    })
+# def profile(request, user=None):
+#     if user is None:
+#         current_user = get_user(request)
+#         if current_user is None:
+#             raise Http404
+#         else:
+#             user = current_user.id
+#     coder = Person.objects.using('datatracker').get(id=user)
+#     email = Email.objects.using('datatracker').filter(person_id=user)
+#     projects = ProjectContainer.objects.exclude(code_request__isnull=True).exclude(is_deleted=True).filter(owner=user)
+#     codings = CodingProject.objects.filter(coder=user)
+#     all_projects = ProjectContainer.objects.all()
+#     selected_codings = []
+#     for proj in all_projects:
+#         all_codings = proj.codings.all()
+#         for code in codings:
+#             if code in all_codings:
+#                 selected_codings.append((proj, code))
+#     return render_page(request, constants.TEMPLATE_PROFILE, {
+#         'coder': coder,
+#         'email': email,
+#         'projects': projects,
+#         'codings': selected_codings,
+#     })
 
 
 def top_coders(request):
--- ietf/codestand/views.py
+++ ietf/codestand/views.py
@@ -25,69 +25,69 @@ def back(request):
     return HttpResponseRedirect(template)
     
     
-@login_required(login_url=settings.CODESTAND_PREFIX + constants.TEMPLATE_LOGIN)    
-def dashboard(request):
-    user = get_user(request)
-    # TODO: Change this screen to when user is not logged
-    if not user:
-        return render_page(request, constants.TEMPLATE_DASHBOARD, {'logged': False})
-    if constants.DASHBOARD_ITEMS not in request.session:
-        items = []
-        keys = {}
-        config = DashboardConfig.objects.filter(user=user.id)
-        if config:
-            data = config[0].data
-            print data
-        else:
-            # TODO: Improve this
-            dash_model = DashboardConfig()
-            dash_model.user = user.id
-            dash_model.data = ''
-            dash_model.save()
-            data = ''
-    else:
-        keys = request.session[constants.DASHBOARD_ITEMS]
-        items = keys['items']
-        data = request.GET.get('request_data')
-    
-    dashconfig = DashboardConfig.objects.get(user=user.id)
-    
-    all_options = [('all_matches', 'All Matches', get_all_matches), 
-                   ('my_requests', 'My Requests', get_my_requests), 
-                   ('my_matches', 'My Matches', get_my_matches)]
-    
-    if data:
-        for option in all_options:
-            if option[0] in data:
-                value = option[2](user,keys)
-                if value in items:
-                    items.remove(value)
-                    dashconfig.data = dashconfig.data.replace(value + ';', '')
-                else:
-                    items.append(value)
-                    if value not in dashconfig.data:
-                        dashconfig.data += value + ';'
-        
-    keys['items'] = items
-    keys['dev'] = True
-    keys['logged'] = True
-    
-    dashconfig.save()
-    
-    options = []
-    for option in all_options:
-        matching = [s for s in items if option[0] in s]
-        if matching:
-            options.append((option[1], "request_access('{}')".format(option[0]), 'color:red'))
-        else:
-            options.append((option[1], "request_access('{}')".format(option[0]), None))
-        
-    keys['options'] = options
-    
-    request.session[constants.DASHBOARD_ITEMS] = keys
-    request.session[constants.MAINTAIN_STATE] = True
-    
-    return render_page(request, constants.TEMPLATE_DASHBOARD, keys)
+# @login_required(login_url=settings.CODESTAND_PREFIX + constants.TEMPLATE_LOGIN)    
+# def dashboard(request):
+#     user = get_user(request)
+#     # TODO: Change this screen to when user is not logged
+#     if not user:
+#         return render_page(request, constants.TEMPLATE_DASHBOARD, {'logged': False})
+#     if constants.DASHBOARD_ITEMS not in request.session:
+#         items = []
+#         keys = {}
+#         config = DashboardConfig.objects.filter(user=user.id)
+#         if config:
+#             data = config[0].data
+#             print data
+#         else:
+#             # TODO: Improve this
+#             dash_model = DashboardConfig()
+#             dash_model.user = user.id
+#             dash_model.data = ''
+#             dash_model.save()
+#             data = ''
+#     else:
+#         keys = request.session[constants.DASHBOARD_ITEMS]
+#         items = keys['items']
+#         data = request.GET.get('request_data')
+#     
+#     dashconfig = DashboardConfig.objects.get(user=user.id)
+#     
+#     all_options = [('all_matches', 'All Matches', get_all_matches), 
+#                    ('my_requests', 'My Requests', get_my_requests), 
+#                    ('my_matches', 'My Matches', get_my_matches)]
+#     
+#     if data:
+#         for option in all_options:
+#             if option[0] in data:
+#                 value = option[2](user,keys)
+#                 if value in items:
+#                     items.remove(value)
+#                     dashconfig.data = dashconfig.data.replace(value + ';', '')
+#                 else:
+#                     items.append(value)
+#                     if value not in dashconfig.data:
+#                         dashconfig.data += value + ';'
+#         
+#     keys['items'] = items
+#     keys['dev'] = True
+#     keys['logged'] = True
+#     
+#     dashconfig.save()
+#     
+#     options = []
+#     for option in all_options:
+#         matching = [s for s in items if option[0] in s]
+#         if matching:
+#             options.append((option[1], "request_access('{}')".format(option[0]), 'color:red'))
+#         else:
+#             options.append((option[1], "request_access('{}')".format(option[0]), None))
+#         
+#     keys['options'] = options
+#     
+#     request.session[constants.DASHBOARD_ITEMS] = keys
+#     request.session[constants.MAINTAIN_STATE] = True
+#     
+#     return render_page(request, constants.TEMPLATE_DASHBOARD, keys)
 
 
 def handler500(request):
