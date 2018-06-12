a6496079740ae2479e0c10b6366bbcffd7ae32a1
osboxes.org <neimar_bbraga@hotmail.com>
2018-06-11 21:30:52 -0300

    affiliation column droped

--- ietf/person/models.py
+++ ietf/person/models.py
@@ -31,7 +31,8 @@ class PersonInfo(models.Model):
     ascii = models.CharField("Full Name (ASCII)", max_length=255, help_text="Name as rendered in ASCII (Latin, unaccented) characters.")
     # The short ascii-form of the name.  Also in alias table if non-null
     ascii_short = models.CharField("Abbreviated Name (ASCII)", max_length=32, null=True, blank=True, help_text="Example: A. Nonymous.  Fill in this with initials and surname only if taking the initials and surname of the ASCII name above produces an incorrect initials-only form. (Blank is OK).")
-    affiliation = models.CharField(max_length=255, blank=True, help_text="Employer, university, sponsor, etc.")
+    #Temp Fix-person_person.Affiliation was dropped on datatracker database -- Neimar Braga 11/06/2018
+    affiliation = ''#models.CharField(max_length=255, blank=True, help_text="Employer, university, sponsor, etc.")
     address = models.TextField(max_length=255, blank=True, help_text="Postal mailing address.")
     biography = models.TextField(blank=True, help_text="Short biography for use on leadership pages. Use plain text or reStructuredText markup.")
     photo = models.ImageField(storage=NoLocationMigrationFileSystemStorage(), upload_to=settings.PHOTOS_DIRNAME, blank=True, default=None)
